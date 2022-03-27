066de5bc376651cd4c36294bc50cf429b0e2a651
shi0280 <sho_ko504128@yahoo.co.jp>
2022-03-27 20:20:38 +0900

    edit mhsendmail

--- app/views/todo/testmail.php
+++ app/views/todo/testmail.php
@@ -1,8 +1,21 @@
 <?php
+
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+
 $from = 'from@example.com';
+$header = "From: $from \n";
+$header = $header
+    . "MIME-Version: 1.0\r\n"
+    . "Content-Transfer-Encoding: 8bit\r\n"
+    . "Content-Type: text/plain; charset=UTF-8\r\n";
 $to   = 'to@example.com';
 $subject = 'テストメール';
 $body = 'メールの送信テストです。';
 
-$ret = mb_send_mail($to, $subject, $body, "From: {$from} \r\n");
+$body = mb_convert_encoding($body, "UTF-8");
+
+$ret = mb_send_mail($to, $subject, $body, $header);
 var_dump($ret);
+echo $body;
+echo $subject;
